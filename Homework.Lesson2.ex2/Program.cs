5789467fc61d619dcb00d1dee5c33dcf530551a7
DmitriySokolov <frakinf999@gmail.com>
2024-01-05 12:28:40 +0500

    написал код для второго задания

---
+++ Homework.Lesson2.ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+static void Main()
+    {
+     Console.Write("Введите координаты точки X и Y:");string[] coordinates = Console.ReadLine().Split(' ');
+     int x = Convert.ToInt32(coordinates[0]);
+     int y = Convert.ToInt32(coordinates[1]);
+        if (x > 0 && y > 0)
+        Console.WriteLine("Точка находится в 1 четвери");
+            else if (x < 0 && y > 0)
+            Console.WriteLine("Точка находится во 2 четвери");
+            else if (x < 0 && y < 0)
+            Console.WriteLine("Точка находится в 3 четвери");
+            else if (x > 0 && y < 0)
+            Console.WriteLine("Точка находится в 4 четвери");
+        else
+        Console.WriteLine("Точка находится на оси координат");
+    }
+}
